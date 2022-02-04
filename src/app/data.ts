a6eb468378e3f6b2100804c3b44c68be120be934
Gugo888 <kroyangurgen@gmail.com>
2022-02-04 17:18:36 +0300

    round 1

---
+++ src/app/data.ts
@@ -0,0 +1,709 @@
+export const CONTACTS = [
+    {
+        "firstName": "Maximus",
+        "lastName": "Hansen",
+        "number": "467-213-5778"
+    },
+    {
+        "firstName": "Leda",
+        "lastName": "Barton",
+        "number": "(965) 765-7743 x501"
+    },
+    {
+        "firstName": "Dimitri",
+        "lastName": "Kulas",
+        "number": "(845) 485-0881"
+    },
+    {
+        "firstName": "Orpha",
+        "lastName": "MacGyver",
+        "number": "723.767.4026"
+    },
+    {
+        "firstName": "Fatima",
+        "lastName": "Schamberger",
+        "number": "633.440.8470 x93967"
+    },
+    {
+        "firstName": "Jedidiah",
+        "lastName": "Stiedemann",
+        "number": "423.292.5645"
+    },
+    {
+        "firstName": "Shanel",
+        "lastName": "Lesch",
+        "number": "1-265-357-1098 x4392"
+    },
+    {
+        "firstName": "Abagail",
+        "lastName": "Abernathy",
+        "number": "913-432-7678"
+    },
+    {
+        "firstName": "Jon",
+        "lastName": "Steuber",
+        "number": "203-786-8075 x56275"
+    },
+    {
+        "firstName": "Theresia",
+        "lastName": "Nienow",
+        "number": "(875) 230-2165"
+    },
+    {
+        "firstName": "Laurence",
+        "lastName": "Becker",
+        "number": "(244) 957-9077"
+    },
+    {
+        "firstName": "Delphine",
+        "lastName": "Corwin",
+        "number": "1-923-204-2109 x97889"
+    },
+    {
+        "firstName": "Robin",
+        "lastName": "Emmerich",
+        "number": "571-648-4860"
+    },
+    {
+        "firstName": "Grant",
+        "lastName": "Kris",
+        "number": "241-922-5898 x29894"
+    },
+    {
+        "firstName": "Gladys",
+        "lastName": "Lang",
+        "number": "1-794-909-7329"
+    },
+    {
+        "firstName": "Amani",
+        "lastName": "Goodwin",
+        "number": "1-231-705-6404"
+    },
+    {
+        "firstName": "Kyler",
+        "lastName": "Gulgowski",
+        "number": "(659) 704-8966"
+    },
+    {
+        "firstName": "Sheldon",
+        "lastName": "Bosco",
+        "number": "523-410-4263 x0273"
+    },
+    {
+        "firstName": "Georgiana",
+        "lastName": "Hauck",
+        "number": "(717) 500-3256"
+    },
+    {
+        "firstName": "Nils",
+        "lastName": "Sipes",
+        "number": "390-608-8031"
+    },
+    {
+        "firstName": "Jakob",
+        "lastName": "Batz",
+        "number": "827-510-4645 x4766"
+    },
+    {
+        "firstName": "Pearl",
+        "lastName": "Breitenberg",
+        "number": "(806) 473-4762 x5891"
+    },
+    {
+        "firstName": "Emmett",
+        "lastName": "Cummerata",
+        "number": "925-612-1876"
+    },
+    {
+        "firstName": "Augustine",
+        "lastName": "Lubowitz",
+        "number": "(976) 912-2437"
+    },
+    {
+        "firstName": "Buford",
+        "lastName": "Skiles",
+        "number": "(984) 595-9174 x154"
+    },
+    {
+        "firstName": "Dagmar",
+        "lastName": "Barton",
+        "number": "1-584-773-6299 x559"
+    },
+    {
+        "firstName": "Moses",
+        "lastName": "Yost",
+        "number": "(449) 450-5320 x5355"
+    },
+    {
+        "firstName": "Vivian",
+        "lastName": "Hoppe",
+        "number": "998-945-0328 x88803"
+    },
+    {
+        "firstName": "Alexie",
+        "lastName": "Schuster",
+        "number": "329.398.0290 x89220"
+    },
+    {
+        "firstName": "Bertha",
+        "lastName": "Walsh",
+        "number": "718.966.8449"
+    },
+    {
+        "firstName": "Marta",
+        "lastName": "Kiehn",
+        "number": "615-436-4148 x5977"
+    },
+    {
+        "firstName": "Ryann",
+        "lastName": "Nikolaus",
+        "number": "1-401-574-2845 x561"
+    },
+    {
+        "firstName": "Sonya",
+        "lastName": "Abshire",
+        "number": "713.462.2532 x9887"
+    },
+    {
+        "firstName": "Zackery",
+        "lastName": "Franey",
+        "number": "475.502.1552 x5598"
+    },
+    {
+        "firstName": "Maryse",
+        "lastName": "Brakus",
+        "number": "232.255.1825"
+    },
+    {
+        "firstName": "Amalia",
+        "lastName": "Larson",
+        "number": "747.410.9745 x19125"
+    },
+    {
+        "firstName": "Monty",
+        "lastName": "Cronin",
+        "number": "603-690-6893"
+    },
+    {
+        "firstName": "Krystel",
+        "lastName": "Armstrong",
+        "number": "(554) 417-5406 x01346"
+    },
+    {
+        "firstName": "Roscoe",
+        "lastName": "Simonis",
+        "number": "1-726-566-2502 x909"
+    },
+    {
+        "firstName": "Jarred",
+        "lastName": "Batz",
+        "number": "546-816-5637 x5473"
+    },
+    {
+        "firstName": "Elenor",
+        "lastName": "Murphy",
+        "number": "1-926-424-7436"
+    },
+    {
+        "firstName": "Charlotte",
+        "lastName": "Sauer",
+        "number": "(296) 656-5814"
+    },
+    {
+        "firstName": "Cristian",
+        "lastName": "Welch",
+        "number": "874-211-5183 x389"
+    },
+    {
+        "firstName": "Asa",
+        "lastName": "Satterfield",
+        "number": "601-731-5981"
+    },
+    {
+        "firstName": "Allene",
+        "lastName": "Ruecker",
+        "number": "928.533.6368"
+    },
+    {
+        "firstName": "Bettye",
+        "lastName": "Brekke",
+        "number": "1-992-676-8507 x05724"
+    },
+    {
+        "firstName": "Olga",
+        "lastName": "Jacobson",
+        "number": "503.921.6376 x23355"
+    },
+    {
+        "firstName": "Elinore",
+        "lastName": "Rosenbaum",
+        "number": "394.869.0660"
+    },
+    {
+        "firstName": "Monserrat",
+        "lastName": "Renner",
+        "number": "898.517.4822 x787"
+    },
+    {
+        "firstName": "Danial",
+        "lastName": "Crona",
+        "number": "255-653-3793 x2730"
+    }
+]
+
+
+export const TODOS = [
+    {
+        "name": "bake",
+        "deadline": "2022-07-30T10:31:18.209Z",
+        "priority": 1
+    },
+    {
+        "name": "lollop",
+        "deadline": "2022-08-23T14:20:44.901Z",
+        "priority": 1
+    },
+    {
+        "name": "evangelize",
+        "deadline": "2022-08-12T01:39:06.539Z",
+        "priority": 5
+    },
+    {
+        "name": "uncoil",
+        "deadline": "2022-07-19T09:40:40.133Z",
+        "priority": 4
+    },
+    {
+        "name": "spin",
+        "deadline": "2022-05-01T06:20:01.196Z",
+        "priority": 3
+    },
+    {
+        "name": "blindfold",
+        "deadline": "2022-02-24T22:21:06.894Z",
+        "priority": 1
+    },
+    {
+        "name": "corrode",
+        "deadline": "2023-01-17T19:18:08.761Z",
+        "priority": 1
+    },
+    {
+        "name": "detach",
+        "deadline": "2022-08-11T22:09:56.049Z",
+        "priority": 4
+    },
+    {
+        "name": "deliberate",
+        "deadline": "2022-03-30T20:10:49.380Z",
+        "priority": 2
+    },
+    {
+        "name": "swivel",
+        "deadline": "2022-06-16T19:47:25.193Z",
+        "priority": 4
+    },
+    {
+        "name": "gaze",
+        "deadline": "2023-01-13T20:03:28.847Z",
+        "priority": 4
+    },
+    {
+        "name": "crank",
+        "deadline": "2022-03-26T15:12:35.713Z",
+        "priority": 4
+    },
+    {
+        "name": "refuse",
+        "deadline": "2023-01-18T05:43:48.194Z",
+        "priority": 4
+    },
+    {
+        "name": "wine",
+        "deadline": "2022-03-28T20:29:15.507Z",
+        "priority": 5
+    },
+    {
+        "name": "deck",
+        "deadline": "2022-11-13T10:10:30.276Z",
+        "priority": 4
+    },
+    {
+        "name": "blind",
+        "deadline": "2023-01-17T14:00:16.217Z",
+        "priority": 4
+    },
+    {
+        "name": "poleax",
+        "deadline": "2022-04-19T08:24:21.201Z",
+        "priority": 2
+    },
+    {
+        "name": "oxidise",
+        "deadline": "2022-03-10T13:48:32.822Z",
+        "priority": 2
+    },
+    {
+        "name": "wire",
+        "deadline": "2022-10-16T07:26:43.884Z",
+        "priority": 4
+    },
+    {
+        "name": "contribute",
+        "deadline": "2022-07-11T18:33:50.705Z",
+        "priority": 3
+    },
+    {
+        "name": "torture",
+        "deadline": "2022-05-31T18:37:50.627Z",
+        "priority": 3
+    },
+    {
+        "name": "hypnotise",
+        "deadline": "2022-11-10T05:44:57.590Z",
+        "priority": 4
+    },
+    {
+        "name": "excoriate",
+        "deadline": "2022-11-27T11:00:12.417Z",
+        "priority": 4
+    },
+    {
+        "name": "insist",
+        "deadline": "2023-01-04T01:15:15.841Z",
+        "priority": 3
+    },
+    {
+        "name": "sue",
+        "deadline": "2022-12-06T01:21:52.689Z",
+        "priority": 4
+    },
+    {
+        "name": "load",
+        "deadline": "2023-01-22T16:57:27.232Z",
+        "priority": 3
+    },
+    {
+        "name": "deduce",
+        "deadline": "2022-06-01T09:03:45.828Z",
+        "priority": 2
+    },
+    {
+        "name": "commute",
+        "deadline": "2022-08-15T17:47:38.835Z",
+        "priority": 3
+    },
+    {
+        "name": "hug",
+        "deadline": "2022-03-24T08:38:06.561Z",
+        "priority": 1
+    },
+    {
+        "name": "towel",
+        "deadline": "2022-07-12T19:30:55.821Z",
+        "priority": 2
+    },
+    {
+        "name": "snarf",
+        "deadline": "2022-08-26T11:08:13.756Z",
+        "priority": 4
+    },
+    {
+        "name": "manure",
+        "deadline": "2022-03-01T03:14:51.688Z",
+        "priority": 4
+    },
+    {
+        "name": "air",
+        "deadline": "2022-12-30T17:37:16.435Z",
+        "priority": 3
+    },
+    {
+        "name": "commend",
+        "deadline": "2022-11-30T00:22:12.254Z",
+        "priority": 2
+    },
+    {
+        "name": "esteem",
+        "deadline": "2022-03-08T17:22:11.840Z",
+        "priority": 4
+    },
+    {
+        "name": "hurtle",
+        "deadline": "2022-09-08T06:33:25.620Z",
+        "priority": 3
+    },
+    {
+        "name": "pooh",
+        "deadline": "2022-02-25T08:42:09.913Z",
+        "priority": 5
+    },
+    {
+        "name": "catapult",
+        "deadline": "2022-08-05T12:41:01.333Z",
+        "priority": 4
+    },
+    {
+        "name": "audition",
+        "deadline": "2022-02-12T14:22:25.716Z",
+        "priority": 3
+    },
+    {
+        "name": "dash",
+        "deadline": "2022-12-30T02:54:12.926Z",
+        "priority": 1
+    },
+    {
+        "name": "gestate",
+        "deadline": "2022-05-04T18:42:27.212Z",
+        "priority": 3
+    },
+    {
+        "name": "barnstorm",
+        "deadline": "2023-01-30T03:25:23.573Z",
+        "priority": 3
+    },
+    {
+        "name": "semaphore",
+        "deadline": "2022-10-21T07:01:54.896Z",
+        "priority": 2
+    },
+    {
+        "name": "transliterate",
+        "deadline": "2022-07-01T05:08:23.793Z",
+        "priority": 3
+    },
+    {
+        "name": "nuzzle",
+        "deadline": "2022-11-30T14:17:35.741Z",
+        "priority": 2
+    },
+    {
+        "name": "emerge",
+        "deadline": "2022-10-28T05:26:28.030Z",
+        "priority": 3
+    },
+    {
+        "name": "sellotape",
+        "deadline": "2022-11-16T02:45:28.644Z",
+        "priority": 3
+    },
+    {
+        "name": "smooch",
+        "deadline": "2022-12-26T15:05:11.173Z",
+        "priority": 5
+    },
+    {
+        "name": "outgun",
+        "deadline": "2022-02-11T04:26:57.692Z",
+        "priority": 3
+    },
+    {
+        "name": "fuck",
+        "deadline": "2022-06-20T15:23:43.976Z",
+        "priority": 1
+    }
+]
+
+export const PHOTOS = [
+    {
+        "url": "http://placeimg.com/640/480/people",
+        "size": 195
+    },
+    {
+        "url": "http://placeimg.com/640/480/city",
+        "size": 446
+    },
+    {
+        "url": "http://placeimg.com/640/480/city",
+        "size": 412
+    },
+    {
+        "url": "http://placeimg.com/640/480/animals",
+        "size": 393
+    },
+    {
+        "url": "http://placeimg.com/640/480/business",
+        "size": 243
+    },
+    {
+        "url": "http://placeimg.com/640/480/nature",
+        "size": 294
+    },
+    {
+        "url": "http://placeimg.com/640/480/technics",
+        "size": 199
+    },
+    {
+        "url": "http://placeimg.com/640/480/food",
+        "size": 275
+    },
+    {
+        "url": "http://placeimg.com/640/480/transport",
+        "size": 301
+    },
+    {
+        "url": "http://placeimg.com/640/480/animals",
+        "size": 369
+    },
+    {
+        "url": "http://placeimg.com/640/480/business",
+        "size": 194
+    },
+    {
+        "url": "http://placeimg.com/640/480/abstract",
+        "size": 464
+    },
+    {
+        "url": "http://placeimg.com/640/480/fashion",
+        "size": 190
+    },
+    {
+        "url": "http://placeimg.com/640/480/people",
+        "size": 323
+    },
+    {
+        "url": "http://placeimg.com/640/480/city",
+        "size": 351
+    },
+    {
+        "url": "http://placeimg.com/640/480/nature",
+        "size": 220
+    },
+    {
+        "url": "http://placeimg.com/640/480/city",
+        "size": 270
+    },
+    {
+        "url": "http://placeimg.com/640/480/nature",
+        "size": 281
+    },
+    {
+        "url": "http://placeimg.com/640/480/animals",
+        "size": 425
+    },
+    {
+        "url": "http://placeimg.com/640/480/nightlife",
+        "size": 387
+    },
+    {
+        "url": "http://placeimg.com/640/480/city",
+        "size": 276
+    },
+    {
+        "url": "http://placeimg.com/640/480/sports",
+        "size": 325
+    },
+    {
+        "url": "http://placeimg.com/640/480/nature",
+        "size": 235
+    },
+    {
+        "url": "http://placeimg.com/640/480/fashion",
+        "size": 410
+    },
+    {
+        "url": "http://placeimg.com/640/480/business",
+        "size": 383
+    },
+    {
+        "url": "http://placeimg.com/640/480/fashion",
+        "size": 178
+    },
+    {
+        "url": "http://placeimg.com/640/480/city",
+        "size": 179
+    },
+    {
+        "url": "http://placeimg.com/640/480/nightlife",
+        "size": 258
+    },
+    {
+        "url": "http://placeimg.com/640/480/animals",
+        "size": 307
+    },
+    {
+        "url": "http://placeimg.com/640/480/abstract",
+        "size": 403
+    },
+    {
+        "url": "http://placeimg.com/640/480/city",
+        "size": 364
+    },
+    {
+        "url": "http://placeimg.com/640/480/animals",
+        "size": 403
+    },
+    {
+        "url": "http://placeimg.com/640/480/business",
+        "size": 304
+    },
+    {
+        "url": "http://placeimg.com/640/480/transport",
+        "size": 403
+    },
+    {
+        "url": "http://placeimg.com/640/480/nature",
+        "size": 458
+    },
+    {
+        "url": "http://placeimg.com/640/480/business",
+        "size": 465
+    },
+    {
+        "url": "http://placeimg.com/640/480/business",
+        "size": 215
+    },
+    {
+        "url": "http://placeimg.com/640/480/fashion",
+        "size": 357
+    },
+    {
+        "url": "http://placeimg.com/640/480/city",
+        "size": 234
+    },
+    {
+        "url": "http://placeimg.com/640/480/cats",
+        "size": 335
+    },
+    {
+        "url": "http://placeimg.com/640/480/sports",
+        "size": 455
+    },
+    {
+        "url": "http://placeimg.com/640/480/transport",
+        "size": 156
+    },
+    {
+        "url": "http://placeimg.com/640/480/technics",
+        "size": 352
+    },
+    {
+        "url": "http://placeimg.com/640/480/abstract",
+        "size": 223
+    },
+    {
+        "url": "http://placeimg.com/640/480/sports",
+        "size": 378
+    },
+    {
+        "url": "http://placeimg.com/640/480/people",
+        "size": 378
+    },
+    {
+        "url": "http://placeimg.com/640/480/animals",
+        "size": 483
+    },
+    {
+        "url": "http://placeimg.com/640/480/people",
+        "size": 338
+    },
+    {
+        "url": "http://placeimg.com/640/480/sports",
+        "size": 365
+    },
+    {
+        "url": "http://placeimg.com/640/480/abstract",
+        "size": 480
+    }
+]
